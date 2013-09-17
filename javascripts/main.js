5cf3e3b554017459f9c43fdc564afe761489aca4
xixilive <xixilive@gmail.com>
2013-09-17 18:20:07 +0800

    update gh-page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,5 @@
-console.log('This would be the main JS file.');
+$(function(){
+  $('#demo1 .region-picker').regionPicker().on('picked.rp', function(e, regions){
+    $('#demo1 input').val(regions.map(function(r){ return r.n; }).join(", "));
+  });
+});
